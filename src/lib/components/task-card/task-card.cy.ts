2e9fe8d122cc94e5117c72eb7ec3d274402ca485
Benjamin Knox <bknox.contact@gmail.com>
2024-10-24 20:24:46 -0400

    [TaskCard]: Add an icons for user story, bug, and planning for default

--- src/lib/components/task-card/task-card.cy.ts
+++ src/lib/components/task-card/task-card.cy.ts
@@ -12,46 +12,72 @@ describe('Task Card', () => {
 				'An example task description that is longer than the amount that should be shown in this card.';
 			const assignee = 'Joe';
 
-      describe('with defaults', () => {
+			describe('with defaults', () => {
+				beforeEach(() => {
+					cy.mount(TaskCard, { props: { title, body, assignee } });
+				});
 
-        beforeEach(() => {
-          cy.mount(TaskCard, { props: { title, body, assignee } });
-        });
+				it('should show the card', () => {
+					cy.get('[data-cy=task-card]').should('exist');
+				});
 
-        it('should show the card', () => {
-          cy.get('[data-cy=task-card]').should('exist');
-        });
+				it('should show the title', () => {
+					cy.get('[data-cy=task-card-title]').should('contain', title);
+				});
 
-        it('should show the title', () => {
-          cy.get('[data-cy=task-card-title]').should('contain', title);
-        });
+				it('should an example text to a line limit of 3', () => {
+					cy.get('[data-cy=task-card-body]')
+						.should('contain', body)
+						.should('have.css', 'line-clamp', '3')
+						.should('have.css', '-webkit-line-clamp', '3')
+						.should('have.css', '-webkit-box-orient', 'vertical')
+						.should('have.css', 'overflow', 'hidden');
+				});
 
-        it('should an example text to a line limit of 3', () => {
-          cy.get('[data-cy=task-card-body]')
-            .should('contain', body)
-            .should('have.css', 'display', '-webkit-box')
-            .should('have.css', 'line-clamp', '3')
-            .should('have.css', '-webkit-line-clamp', '3')
-            .should('have.css', '-webkit-box-orient', 'vertical')
-            .should('have.css', 'overflow', 'hidden');
-        });
+				it('should show an assignee', () => {
+					cy.get('[data-cy=task-card-assignee]').should('contain', `Assigned to ${assignee}`);
+				});
 
-        it('should show an assignee', () => {
-          cy.get('[data-cy=task-card-assignee]').should('contain', `Assigned to ${assignee}`);
-        });
+				it('should show the default task type', () => {
+					cy.get('[data-cy=task-card-type]').should('contain', 'user story');
+				});
 
-        it('should show the default task type', () => {
-          cy.get('[data-cy=task-card-type]').should('contain', 'user story');
-        });
-        });
+				it('should show the user icon', () => {
+					cy.get('[data-cy=task-card-type] [style*=user-story]').should('exist');
+				});
+			});
 
-      describe('with non-default values', () => {
-        it('should show a custom task type', () => {
-          const type = 'non-default';
-          cy.mount(TaskCard, { props: { title, body, assignee, type } });
-          cy.get('[data-cy=task-card-type]').should('contain', type);
-        });
-      });
+			describe('with non-default values', () => {
+				it('should show a custom task type', () => {
+					const type = 'non-default';
+					cy.mount(TaskCard, { props: { title, body, assignee, type } });
+					cy.get('[data-cy=task-card-type]').should('contain', type);
+				});
+
+				it('should default to a plan icon', () => {
+					const type = 'anything';
+					cy.mount(TaskCard, { props: { title, body, assignee, type } });
+					cy.get('[data-cy=task-card-type] [style*=plan]').should('exist');
+				});
+
+				it('should show a user story icon', () => {
+					const type = 'user story';
+					cy.mount(TaskCard, { props: { title, body, assignee, type } });
+					cy.get('[data-cy=task-card-type] [style*=user-story]').should('exist');
+				});
+
+				it('should show a bug fix icon', () => {
+					const type = 'bug fix';
+					cy.mount(TaskCard, { props: { title, body, assignee, type } });
+					cy.get('[data-cy=task-card-type] [style*=bug]').should('exist');
+				});
+
+				it('should show a plan icon', () => {
+					const type = 'planning';
+					cy.mount(TaskCard, { props: { title, body, assignee, type } });
+					cy.get('[data-cy=task-card-type] [style*=plan]').should('exist');
+				});
+			});
 		});
 
 		describe('when inputs are out of range', () => {
@@ -59,7 +85,7 @@ describe('Task Card', () => {
 			const body =
 				'An example task description that is longer than the amount that should be shown in this card.';
 			const assignee = "Joe's name is way too long";
-      const type = "This is a really long type that should be truncated";
+			const type = 'This is a really long type that should be truncated';
 
 			beforeEach(() => {
 				cy.mount(TaskCard, { props: { title, body, assignee, type } });
@@ -80,7 +106,7 @@ describe('Task Card', () => {
 			});
 
 			it('should show an ellipsis on a long type', () => {
-				cy.get('[data-cy=task-card-type]')
+				cy.get('[data-cy=task-card-type] .card-type-text')
 					.should('have.css', 'text-overflow', 'ellipsis')
 					.should('have.css', 'overflow', 'hidden')
 					.should('have.css', 'white-space', 'nowrap');
--- src/lib/components/task-card/task-card.svelte
+++ src/lib/components/task-card/task-card.svelte
@@ -1,58 +1,77 @@
 <script lang="ts">
 	import Container from '@awenovations/aura/container.svelte';
+	import Icon from '@awenovations/aura/icon.svelte';
 
-  export let title: string;
-  export let body: string;
-  export let assignee: string;
-  export let type = "user story";
-
+	export let title: string;
+	export let body: string;
+	export let assignee: string;
+	export let type = 'user story';
 </script>
 
 <Container data-cy="task-card" clearPadding variant="elevated">
-  <div class="card">
-    <h4 class="card-title" data-cy="task-card-title">{title}</h4>
-    <span class="card-body" data-cy="task-card-body">{body}</span>
-    <span class="card-assignee" data-cy="task-card-assignee">Assigned to <i>{assignee}</i></span>
-    <span class="card-type" data-cy="task-card-type">{type}</span>
-  </div>
+	<div class="card">
+		<h4 class="card-title" data-cy="task-card-title">{title}</h4>
+		<span class="card-body" data-cy="task-card-body">{body}</span>
+		<span class="card-assignee" data-cy="task-card-assignee">Assigned to <i>{assignee}</i></span>
+		<span class="card-type" data-cy="task-card-type"
+			><span class="card-type-text">{type}</span>
+			{#if type === 'user story'}
+				<Icon name="user-story" />
+			{:else if type === 'bug fix'}
+				<Icon name="bug" />
+			{:else}
+				<Icon name="plan" />
+			{/if}
+		</span>
+	</div>
 </Container>
 
 <style lang="scss">
-  .card {
-    height: 12.714rem;
-    width: 13.929rem; 
-    padding: 0.857rem;
-    box-sizing: border-box;
-    text-align: left;
-    font-weight: 300;
-    display: flex;
-    flex-direction: column;
-    gap: 1.143rem;
+	.card {
+		height: 12.714rem;
+		width: 13.929rem;
+		padding: 0.857rem;
+		box-sizing: border-box;
+		text-align: left;
+		font-weight: 300;
+		display: flex;
+		flex-direction: column;
+		gap: 1.143rem;
+		justify-content: space-between;
 
-    .card-type,
-    .card-assignee,
-    .card-title {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
+		.card-type-text,
+		.card-assignee,
+		.card-title {
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
 
-    .card-body {
-      display: -webkit-box;
-      line-clamp: 3;
-      -webkit-line-clamp: 3;
-      -webkit-box-orient: vertical;  
-      overflow: hidden;
-    }
+		.card-body {
+			display: -webkit-box;
+			line-clamp: 3;
+			-webkit-line-clamp: 3;
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+		}
 
-    .card-type {
-      text-align: right;
-      margin-top: auto;
-      color: var(--aura-tertiary-50);
-    }
+		.card-type {
+			display: flex;
+			justify-content: space-between;
 
-    i {
-      font-style: italic;
-    }
-  }
+			.card-type-text {
+				text-align: right;
+				margin-top: auto;
+				color: var(--aura-tertiary-50);
+			}
+
+			& :global(.aura-icon .icon) {
+				background: var(--aura-tertiary-50);
+			}
+		}
+
+		i {
+			font-style: italic;
+		}
+	}
 </style>
