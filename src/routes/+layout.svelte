8b6be08f28d28ab641acf0ca8812df6bbeb620b6
Benjamin Knox <bknox.contact@gmail.com>
2026-01-26 05:30:53 -0500

    [Responsive]: Remove small screen message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -109,16 +109,6 @@
 	>&nbsp;| My Website:&nbsp;<Link target="blank" href="https://knox.pro">knox.pro</Link>
 </footer>
 
-<div class="small-screen-message">
-	<Container class="message"
-            >🤔 
-            <br />
-            <br />
-            Your screen is a bit small for this app. This app requires a minimum width of 1000px. Responsiveness is still being developed.</Container
-	>
-	<div class="backdrop"></div>
-</div>
-
 <style lang="scss">
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
@@ -248,28 +238,6 @@
 
 		& :global(a.aura-link:active) {
 			font-size: 8pt;
-		}
-	}
-
-	.small-screen-message {
-		:global(.message) {
-			position: absolute;
-			z-index: 1001;
-			max-width: 25.429rem;
-			margin: auto;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			line-height: 1.5em;
-		}
-
-		width: 100%;
-		height: 100%;
-		position: absolute;
-		top: 0;
-		left: 0;
-		@media (min-width: 71.429rem) {
-			display: none;
 		}
 	}
 </style>
